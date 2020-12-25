0c19da704901f31c498c6c631e160283a3a8b75a
ahmedallani <allania7med1@gmail.com>
2020-12-25 11:38:42 +0100

    grid test

--- src/app/grid-list/grid-list.component.ts
+++ src/app/grid-list/grid-list.component.ts
@@ -13,7 +13,7 @@ export interface Tile {
 })
 export class GridListComponent implements OnInit {
   tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'One', cols: 2, rows: 1, color: 'lightblue'},
     {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
     {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
